25d243a2c1339ec76ac52db46535c2c2dd8fed0e
Anna <118673659+AnnaKuliga@users.noreply.github.com>
2023-08-10 13:30:25 +0200

    added components to the main content component

--- src/components/Body/Body.jsx
+++ src/components/Body/Body.jsx
@@ -1,8 +1,25 @@
 import React from "react";
 import "./Body.css";
+import Hero from "./Hero/Hero";
+import About from "./About/About";
+import Skills from "./Skills/Skills";
+import Projects from "./Projects/Projects";
+import Reviews from "./Reviews/Reviews";
+import Contact from "./Contact/Contact";
 
 const Body = () => {
-  return <div className="body">Body</div>;
+  return (
+    <div className="mainContent">
+      <Hero />
+      <div className="bottom flex">
+        <About />
+        <Skills />
+        <Projects />
+        <Reviews />
+        <Contact />
+      </div>
+    </div>
+  );
 };
 
 export default Body;

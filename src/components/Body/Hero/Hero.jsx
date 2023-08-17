99e5f688ef491f52d2c0ed235a66bdc5c21b2a3b
Anna <118673659+AnnaKuliga@users.noreply.github.com>
2023-08-17 19:30:43 +0200

    added hero content

--- src/components/Body/Hero/Hero.jsx
+++ src/components/Body/Hero/Hero.jsx
@@ -1,7 +1,24 @@
 import React from "react";
 
 const Hero = () => {
-  return <div className="heroDiv">Hello Hero</div>;
+  return (
+    <div className="hero">
+      <div className="heroDiv">
+        <h1>
+          I am <span className="heading-hero">Frontend</span> Developer
+        </h1>
+        <p>I am Anna Kuliga, React Developer, based in Europe.</p>
+        <div className="buttons">
+          <a href="#projects" class="btn" title="Explore">
+            Explore my work
+          </a>
+          <a href="tel:+4792535197" class="btn" title="Call me">
+            Call +47 925 35 197
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Hero;

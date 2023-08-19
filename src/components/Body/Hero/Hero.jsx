b54a4c6b23b150d72322628f62a7ff0576f870cb
Anna <118673659+AnnaKuliga@users.noreply.github.com>
2023-08-19 15:45:36 +0200

    added and styled social links component

--- src/components/Body/Hero/Hero.jsx
+++ src/components/Body/Hero/Hero.jsx
@@ -1,6 +1,12 @@
 import React from "react";
 import "./Hero.css";
 import Header from "../Header/Header";
+
+//imported icons ====>
+import { AiOutlineGithub } from "react-icons/ai";
+import { BiLogoLinkedin } from "react-icons/bi";
+import { IoSchool } from "react-icons/io5";
+
 const Hero = () => {
   return (
     <div className="hero full-height">
@@ -14,7 +20,7 @@ const Hero = () => {
           <h1>
             I am <span class="text-brand">Frontend Developer</span>
           </h1>
-          <p>My name is Anna and I am React Developer, based in Europe.</p>
+          <p>My name is Anna and I am a React Developer, based in Europe.</p>
           <div className="contact">
             <a href="tel:+47 925 35 197" className="tel custom-link">
               Call +47 925 35 197
@@ -22,7 +28,37 @@ const Hero = () => {
           </div>
         </div>
       </div>
-      <div className="right">right</div>
+      <div className="right">
+        <div className="social">
+          <a
+            href="https://github.com/AnnaKuliga"
+            title="Anna Kuliga GitHub "
+            target="_blank"
+            rel="noreferrer"
+            className="social-link"
+          >
+            <AiOutlineGithub className="icon" />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/annakuliga/"
+            title="Anna Kuliga LinkedIn"
+            target="_blank"
+            rel="noreferrer"
+            className="social-link"
+          >
+            <BiLogoLinkedin className="icon" />
+          </a>
+          <a
+            href="https://www.shecodes.io/graduates/57519-anna-kuliga"
+            title="Anna Kuliga SheCodes"
+            target="_blank"
+            rel="noreferrer"
+            className="social-link"
+          >
+            <IoSchool className="icon" />
+          </a>
+        </div>
+      </div>
     </div>
   );
 };

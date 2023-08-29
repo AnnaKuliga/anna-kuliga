2017a05bb224c06ec8cfa7cce8ec722169bf83d9
Anna <118673659+AnnaKuliga@users.noreply.github.com>
2023-08-29 19:44:37 +0200

    added img and the about section content

--- src/components/Body/About/About.jsx
+++ src/components/Body/About/About.jsx
@@ -1,17 +1,40 @@
 import React from "react";
 import "./About.css";
+
+import anna from "../../../assets/img/anna_kul.jpg";
+
 const About = () => {
   return (
     <div className="About full-height" id="about">
       <div className="personal">
-        <h3>About Anna</h3>
-        <p>
-          My name is Anna. I come from a small village in the south-east of
-          Poland. Currently, I live in Oslo, Norway. In 2020, I graduated from
-          Maria Curie- Skłodowska University in Lublin. I have BA and MA in
-          Applied Linguistics. I speak English fluently and also French. My true
-          passion is photography and learning new languages.
-        </p>
+        <div className="p-left">
+          <h3>About Anna</h3>
+          <p className="text">
+            My name is Anna. I come from a small village in the south-east of
+            Poland. Currently, I live in Oslo, Norway. In 2020, I graduated from
+            Maria Curie- Skłodowska University in Lublin. I have BA and MA in
+            Applied Linguistics. I speak English fluently and also French. My
+            true passion is photography and learning new languages.
+          </p>
+          <p className="career">
+            Right after graduating the university, I set up my own company.
+            Youniverse Language Academy is a language school based in
+            podkarpackie region. Recently, I have found myself really into
+            coding. I have been successfully graduating SheCodes online
+            workshops. In my professional life I follow perfection. I always do
+            my best in everything. I am not afraid of challenges. I am a fast
+            learner and personally, I believe that time management skills are
+            essential while pursuing my professional dreams and goals. I value
+            and respect the work and experience of other people. That is why
+            team work is important in my life. It does not only give me an
+            opportunity to work with experienced people, but also I can share my
+            knowledge with others.
+          </p>
+        </div>
+        <div className="p-right">
+          <div className=""></div>
+          <img src={anna} alt="Anna Kuliga image" className="rounded-circle" />
+        </div>
       </div>
     </div>
   );

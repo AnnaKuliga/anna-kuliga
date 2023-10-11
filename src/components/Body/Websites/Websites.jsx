d58719282104df83ecc3017f3bc662cf8a2d5064
Anna <118673659+AnnaKuliga@users.noreply.github.com>
2023-10-11 10:10:16 +0200

    added website project content

--- src/components/Body/Websites/Websites.jsx
+++ src/components/Body/Websites/Websites.jsx
@@ -10,11 +10,17 @@ const Websites = () => {
           This project is meant for mostly local, small companies which lack
           online websites. Every small company or a freelancer can sign for the
           project. It is about creating a fully responsive website. There are no
-          limits. Every comany or every person who is working actively can sign
+          limits. Every company or every person who is working actively can sign
           in.{" "}
         </p>
         <div className="benefit-list">
-          <h4>What you'll get:</h4>
+          <h4 className="stroke-text">What you'll get:</h4>
+          <ul>
+            <li className="benefit-item">amazing design of the page</li>
+            <li className="benefit-item">fully responsive website </li>
+            <li className="benefit-item">hosting is also included</li>
+            <li className="benefit-item">satisfaction and more clients</li>
+          </ul>
         </div>
       </div>
     </div>

d1b6a98187753f544979c0f49777d5daf28ab4f9
Anna <118673659+AnnaKuliga@users.noreply.github.com>
2023-10-11 11:16:58 +0200

    added contact content

--- src/components/Body/Contact/Contact.jsx
+++ src/components/Body/Contact/Contact.jsx
@@ -1,11 +1,41 @@
 import React from "react";
 import "./Contact.css";
 
+//imported icons ---->
+import { HiOutlineMailOpen } from "react-icons/hi";
+import { BsTelephone } from "react-icons/bs";
+
 const Contact = () => {
   return (
     <div className="Contact" id="contact">
-      <div className="contact-left">LEFT</div>
-      <div className="contact-right">RIGHT</div>
+      <div className="contact-info">
+        <h2>Contact information</h2>
+        <h6>
+          If you like my projects and want to work together, you can call or
+          send me an email.
+        </h6>
+        <h6 class="zone">CET Time Zone</h6>
+        <div className="phone">
+          <p>
+            <a href="tel:+48 785 036 017" title="Poland">
+              <BsTelephone />
+              +48 785 036 017
+            </a>
+          </p>
+          <p>
+            <a href="tel:+47 925 35 197" title="Norway">
+              <BsTelephone />
+              +47 925 35 197
+            </a>
+          </p>
+          <p>
+            <a href="mailto:a.kuliga2@gmail.com" title="Email me">
+              <HiOutlineMailOpen />
+              <span className="email"> a.kuliga2@gmail.com</span>
+            </a>
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
